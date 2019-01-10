cc1dd4e59e74ab353d79590de7598b793832d2ac
danielrbates <danielrbates@gmail.com>
2019-01-09 19:00:36 -0500

    Add files via upload
    Fixed numerous issues resulting in a bad implementation of compare-object.

--- Rootkit Hunter.ps1
+++ Rootkit Hunter.ps1
@@ -1,35 +1,34 @@
-﻿function Get-pslist-processes
+﻿$path=$env:USERPROFILE
+
+function Get-pslist-processes
 {
 $pslist = & C:\Sysinternals\pslist.exe -accepteula
 $pslist = $pslist[8..($pslist.length-1)]
 $pslist | foreach {
-    $columns = $_ -split "\s{2,}", 8
+    $columns = $_ -split '(\s+\b(?=\d))'
     New-Object -TypeName PSObject -Property @{
         ProcessName = $columns[0]
-        ID = [int]$columns[1]
-        Priority = $columns[2]
-        Threads = $columns[3]
-        Handles = $columns[4]
-        Private = $columns[5]
-        CPUTime = $columns[6]
-        ElapsedTime = $columns[7]
+        ID = [int]$columns[2]
         }
     }
 }
 
-$internal = (Get-Process | select ID, ProcessName | Sort-Object -Property ID)
-$external = (Get-pslist-processes | select ID, ProcessName | Sort-Object -Property ID)
+$internal = (Get-Process | select ID,ProcessName | Sort-Object -Property ID)
+notepad
+$external = (Get-pslist-processes | select ID,ProcessName | Sort-Object -Property ID)
+$internal | Out-File -FilePath "$path\internal.txt"
+$external | Out-File -FilePath "$path\external.txt"
 
-$difference = (Compare-Object -ReferenceObject $internal -DifferenceObject $external -PassThru)
+$difference = (Compare-Object -ReferenceObject $(get-content $path\internal.txt) -DifferenceObject $(get-content $path\external.txt) )
 
 echo ""
 echo "PSList counts $($external.count) processes, and Get-Process counts $($internal.count) processes."
 
-foreach ($i in $difference) {
-    #echo "Difference SideIndicator is $($difference.SideIndicator)"
-    if ($difference.SideIndicator -eq "<=") {
-        echo "Process ID $($difference.ID), $($difference.ProcessName), was only found by Get-Process."
+for ($i=0; $i -lt $difference.Length; $i++) {
+    #echo "Difference SideIndicator is $($difference[$i].SideIndicator)"
+    if ($difference[$i].SideIndicator -eq "<=") {
+        echo "Process ID $(($difference[$i].InputObject -split "\s+\b")[0]), $((($difference[$i].InputObject -split "\s+\b")[1]) -split "\s+"), was only found by Get-Process."
         } else {
-        echo "Process ID $($difference.ID), $($difference.ProcessName), was only found by PSList."
+        echo "Process ID $(($difference[$i].InputObject -split "\s+\b")[0]), $((($difference[$i].InputObject -split "\s+\b")[1]) -split "\s+"), was only found by PSList."
         }
     }
