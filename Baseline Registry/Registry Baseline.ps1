cc4d02ef919ebf731d2910e876e1c8184eb5399f
danielrbates <danielrbates@gmail.com>
2019-01-17 10:19:26 -0500

    Add files via upload
    Major improvements to the Baseline and Diff scripts.

--- Baseline Registry/Registry Baseline.ps1
+++ Baseline Registry/Registry Baseline.ps1
@@ -1,9 +1,25 @@
-﻿#Basic system information
-$computername = $env:COMPUTERNAME
-$userprofile = $env:USERPROFILE
+﻿# Set machine status (clean or infected):
+# (This will become part of the output filename)
+$machine_status = "clean"
 
-#Set output file
-$outfile = "$userprofile\registry baseline TESTONLY.txt"
+#Set output file location and filename, and create the file
+$outfile = "$userprofile\registry baseline - $machine_status.txt"
+New-Item -Path $outfile -ItemType File -Force
+
+# Define interesting keys
+$keylist = @(
+    # Auto-run locations for the machine and individual users
+    "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run",
+    "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce",
+    "HKU:\*\SOFTWARE\Microsoft\Windows\CurrentVersion\Run",
+    "HKU:\*\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce"
+    # Recent network connections
+    "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Profiles\*\"
+    # User accounts
+    "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\*\" 
+    # Connected USB devices
+    "HKLM:\SYSTEM\CurrentControlSet\Enum\USB\*\*\Device Parameters"
+    )
 
 #Set registry path for HKEY_USERS, if it doesn't exist
 If (!(test-path HKU:)) {
@@ -11,37 +27,41 @@ If (!(test-path HKU:)) {
     Set-Location HKU:
     }
 
-#Get forensically relevant registry keys
-##Persistence  -get item properties for the given registry path
-#              -deselect the built-in PowerShell properties
-#              -format as a list 
-#              -convert to an array of strings
-#              -remove blank lines
-$HKLMrun = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" | select * -ExcludeProperty PS* | Format-List | Out-String -Stream | Where-Object {$_.trim() -ne "" }
-$HKLMrunonce = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" | select * -ExcludeProperty PS* | Format-List | Out-String -Stream | Where-Object {$_.trim() -ne "" }
-$HKUrun = Get-ItemProperty -Path "HKU:\*\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" | select * -ExcludeProperty PS* | Format-List | Out-String -Stream | Where-Object {$_.trim() -ne "" }
-$HKUrunonce = Get-ItemProperty -Path "HKU:\*\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" | select * -ExcludeProperty PS* | Format-List | Out-String -Stream | Where-Object {$_.trim() -ne "" }
-##Recent network connections
-$NetworkProfile = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Profiles\*\"
-##User accounts
-$UserAccounts = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\*\" 
-##Connected USB devices
-$USB = Get-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Enum\USB\*\*\Device Parameters"
+# Function Definitions
+# Parse a registry key, given a path [string].  Prints the path, key
+# name, data, and type to the output file.
+function parse-registrykey {
+    param([string]$path)
+    $key = get-item -path $path
+    foreach ($subkey in $key.Property) {
+        echo ("$($key.PSPath.Split('::')[2]),$subkey,$($key.GetValue("$subkey")),$($key.GetValueKind("$subkey"))") | out-file $outfile -Append
+        }
+    }
+# Parse a registry key that contains subkeys.  Takes a path [string]
+# as input and passes each subkey path on to the parsing function.
+function parse-wildcardedregistrykey {
+    param([string]$path)
+    $key = get-item -path $path
+    foreach ($item in $key.PSPath) {
+        parse-registrykey -path $item
+        }
+    }
 
-#Export information to a text file
-echo "Baseline Registry Configuration for $computername`n" | Out-File $outfile
-$(echo "","","Persistence Keys","--------------------------------------------------") | Out-File $outfile -Append
-echo ("HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run",$HKLMrun,"") | Out-File $outfile -Append
-echo ("HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce",$HKLMrunonce,"") | Out-File $outfile -Append
-echo ("HKU:\*\SOFTWARE\Microsoft\Windows\CurrentVersion\Run",$HKUrun,"") | Out-File $outfile -Append
-echo ("HKU:\*\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce",$HKUrunonce,"") | Out-File $outfile -Append
-$(echo "","","Recent Network Connections","--------------------------------------------------") | Out-File $outfile -Append
-echo ($NetworkProfile.ProfileName,"") | Out-File $outfile -Append
-$(echo "","","User Accounts","--------------------------------------------------") | Out-File $outfile -Append
-echo ($UserAccounts.ProfileImagePath,"") | Out-File $outfile -Append
-$(echo "","","USB devices","--------------------------------------------------") | Out-File $outfile -Append
-echo ($USB,"") | Out-File $outfile -Append
+#Main Function
+#####################################################################
+# Parse interesting keys, defined earlier:
+# (Wildcarded key paths must be processed through a separate function)
+foreach ($item in $keylist) {
+    if (($item -match '\*') -eq $true) {
+        # If true, this path contains a wildcard.  Go to the function
+        # that can handle a wildcarded path:
+        parse-wildcardedregistrykey -path $item
+    }else{
+        # Otherwise, it does not contain a wildcard.  Go directly to
+        # the parser function:
+        parse-registrykey -path $item
+        }
+    }
 
-# Automatically open the output file
-# (Comment out if not needed)
+# Automatically open the output file (comment out if not needed)
 notepad $outfile
