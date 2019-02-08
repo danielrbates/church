ebe682a908ce6f47e829f455f16cf69d9e6aa3f2
danielrbates <danielrbates@gmail.com>
2019-02-08 10:57:04 -0500

    Update and rename firewall_rules.ps1 to Set Firewall Rules.ps1

---
+++ Set Firewall Rules.ps1
@@ -0,0 +1,28 @@
+﻿function create-fwrules {
+    Set-NetFirewallRule -DisplayName "Block Wireless" -InterfaceType Wireless -Direction Outbound -Action Block
+    Set-NetFirewallRule -DisplayName "Block TFTP src" -Protocol UDP -LocalPort 69 -Direction Outbound -Action Block
+    Set-NetFirewallRule -DisplayName "Block TFTP dest" -Protocol UDP -RemotePort 69 -Direction Outbound -Action Block
+    Set-NetFirewallRule -DisplayName "Block DNS-UDP" -Protocol UDP -RemotePort 53 -Direction Outbound -Action Block
+    Set-NetFirewallRule -DisplayName "Block DNS-TCP" -Protocol TCP -RemotePort 53 -Direction Outbound -Action Block
+    Set-NetFirewallRule -DisplayName "Block Application" -Program "path\path\executable.exe" -Direction Outbound -Action Block
+    }
+
+function delete-fwrules {
+    Remove-NetFirewallRule -DisplayName "Block Wireless"
+    Remove-NetFirewallRule -DisplayName "Block TFTP src"
+    Remove-NetFirewallRule -DisplayName "Block TFTP dest"
+    Remove-NetFirewallRule -DisplayName "Block DNS-UDP"
+    Remove-NetFirewallRule -DisplayName "Block DNS-TCP"
+    Remove-NetFirewallRule -DisplayName "Block Application"
+    }
+
+function main {
+    while ($true) {
+        #echo "creating rules"
+        create-fwrules
+        start-sleep -Seconds 60
+        #echo "deleting rules"
+        delete-fwrules
+        start-sleep -Seconds 60
+        }
+    }
